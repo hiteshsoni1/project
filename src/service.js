9f047862f284fa430cdad392d712bf0217b8a30b
hiteshsoni1 <61386145+hiteshsoni1@users.noreply.github.com>
2020-09-18 14:23:45 +0530

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,4 +1,4 @@
-const API_KEY = '92238c1308cfdba1d617e6a61895d886';
+const API_KEY = '3e120b161195c11e5e35d6dac8a1b548';
 export const getData = async (search, page) => {
     let data = [];
     const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=25&api_key=${API_KEY}&text=${search}&page=${page}&format=json&nojsoncallback=1`;
